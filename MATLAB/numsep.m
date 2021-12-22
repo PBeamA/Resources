b277466a0bef27e0218e314b592278f63dd05f8c
PBeamA <PBeamA@users.noreply.github.com>
2021-12-22 23:48:11 +1300

    Done?

--- MATLAB/numsep.m
+++ MATLAB/numsep.m
@@ -1,22 +1,30 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% numsep.m
+%
+% Beam.Aschakulporn@otago.ac.nz
+% https://pbeama.github.io/
+% Modified: Wednesday 22 December 2021 (23:43)
+% * Comments removed.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function str = numsep(num)
 if ~ischar(num)
-    str = num2str(num);
+str = num2str(num);
 else
-    str = num;
+str = num;
 end
 j = find(str == '.', 1);
 if isempty(j)
-    str = fliplr(str);
-    i = 0;
-    while i < length(str)
-        i = i + 3;
-        if i < length(str)
-            i = i + 1;
-            str = [str(1 : i - 1), ',', str(i : end)];
-        end
-    end
-    str = fliplr(str);
+str = fliplr(str);
+i = 0;
+while i < length(str)
+i = i + 3;
+if i < length(str)
+i = i + 1;
+str = [str(1 : i - 1), ',', str(i : end)];
+end
+end
+str = fliplr(str);
 else
-    str = [numsep(str2double(str(1 : j - 1))), str(j : end)];
+str = [numsep(str2double(str(1 : j - 1))), str(j : end)];
 end
 end
--- MATLAB/shadex.m
+++ MATLAB/shadex.m
@@ -1,9 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% shadex.m
+%
+% Beam.Aschakulporn@otago.ac.nz
+% https://pbeama.github.io/
+% Modified: Wednesday 22 December 2021 (23:43)
+% * Comments removed.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function shadex(x1, x2)
-% Shades current figure from x1 to x2
 hold on
 ylimits = get(gca, 'ylim'); % Get y axis limits
 
-colour = [0.4, 0.4, 0.4];   % Grey
+colour = [0.4, 0.4, 0.4]; % Grey
 p = fill([x1, x2, x2, x1], sort([ylimits, ylimits]), colour);
 set(p, 'FaceAlpha', 0.25, 'EdgeColor', 'none');
 
